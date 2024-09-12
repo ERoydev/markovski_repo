f7e1b0b84258fb7b4563b66788624339df6a7af0
Emil <e.roydev@gmail.com>
2024-09-12 13:23:43 +0300

    removed one useless component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './components/user-list/user-list.component';
-import { UserFormComponent } from './components/user-form/user-form.component';
 import { PaginationModule } from '../shared/components/pagination/pagination.module';
 import { DialogComponent } from '../dialog/dialog.component';
 import { RouterModule } from '@angular/router';
@@ -10,7 +9,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     UserListComponent,
-    UserFormComponent
   ],
   imports: [
     CommonModule,
@@ -20,7 +18,6 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     UserListComponent,
-    UserFormComponent
   ]
 })
 export class UserModule { }
